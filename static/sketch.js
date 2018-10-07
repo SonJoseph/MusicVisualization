0b5f45e5a767188c7dc9ac5934bdcda38b266200
Anna Hattle <annahattle@annas-mbp-5.wireless-1x.unc.edu>
2018-10-06 20:12:37 -0400

    wowww

--- static/sketch.js
+++ static/sketch.js
@@ -6,18 +6,23 @@ var path = "/static/music/happy.mp3"
 var song;
 var button;
 var amplitude;
+var volhistory = [];
+
+// function preload() {
+//   song = loadSound(path)
+// }
 
 function setup() {
   createCanvas(200, 200);
   song = loadSound(path, loaded);
-  amp = new p5.Amplitude();
+  amplitude = new p5.Amplitude();
   background(51);
 }
 
 function togglePlaying() {
   if (!song.isPlaying()) {
     song.play();
-    song.setVolume(0.3);
+    // song.setVolume(1);
     button.html("pause");
   } else {
     song.stop();
@@ -32,9 +37,29 @@ function loaded() {
 
 function draw(){
     background(51);
-    var vol = amp.getLevel();
-    fill(255,0,255);
-    ellipse(width/2, height/2, vol*1000, vol*1000);
+    var vol = amplitude.getLevel();
+    volhistory.push(vol);
+    stroke(255);
+    noFill();
+    
+    translate(0, -height/2);
+    beginShape();
+    for(var i = 0; i < volhistory.length; i++){
+      var y = map(volhistory[i], 0, 1, height, 0);
+      vertex(i,y);
+    }
+    endShape();
+
+    if(volhistory.length > width- 50){
+      volhistory.splice(0,1);
+    }
+
+    stroke(255, 0,0);
+    line(volhistory.length, 0, volhistory.length, height);
+
+    // var diam = map(vol, 0, 0.3, 10, 200);
+    // fill(255,0,255);
+    // ellipse(width/2, height/2, diam, diam);
 }
 
 console.log("hi")
