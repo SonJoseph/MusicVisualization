d34cd9b27a91bd1baa881a1acf274d818f33353e
SonJoseph <joeson34@cs.unc.edu>
2018-10-06 21:07:54 -0400

    two different graphs :D

--- app.py
+++ app.py
@@ -1,9 +1,18 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
+
 @app.route('/')
 def home():
-    return render_template("index.html")
+    return "G'day weary traveler! Go to /amplitudeGraph or /frequency to see some cool stuff ... wow!"
+
+@app.route('/amplitudeGraph')
+def amplitudeGraph():
+    return render_template("amplitudeGraph.html")
+
+@app.route('/frequency')
+def frequency():
+    return render_template("frequency.html")
 
 if __name__ == '__main__':
    app.run('127.0.0.1', 8000, debug = True)
