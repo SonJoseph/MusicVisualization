67d0131d0f696fbf081435db8fb714fffe13b370
Anna Hattle <annahattle@Annas-MacBook-Pro-5.local>
2018-10-11 21:47:32 -0400

    sunset freq

--- static/frequency.js
+++ static/frequency.js
@@ -6,6 +6,7 @@
 var song;
 var fft;
 var button;
+var amplitude;
 
 function toggleSong() {
   if (song.isPlaying()) {
@@ -16,41 +17,56 @@ function toggleSong() {
 }
 
 function preload() {
-  song = loadSound('/static/music/internetgirl.mp3');
+  song = loadSound('/static/music/inyourarms.mp3');
 }
 
 function setup() {
-  createCanvas(256, 256);
+  createCanvas(600, 600);
   colorMode(HSB);
   angleMode(DEGREES);
   button = createButton('toggle');
   button.mousePressed(toggleSong);
   song.play();
-  fft = new p5.FFT(0.9, 128);
+  fft = new p5.FFT(0.9, 64);
+  amplitude = new p5.Amplitude();
+  
 }
 
 function draw() {
-  background(0);
+  background(500);
   var spectrum = fft.analyze();
   //console.log(spectrum);
   //stroke(255);
   noStroke();
   translate(width / 2, height / 2);
   //beginShape();
-  for (var i = 0; i < spectrum.length; i++) {
-    var angle = map(i, 0, spectrum.length, 0, 360);
-    var amp = spectrum[i];
-    var r = map(amp, 0, 256, 20, 100);
-    //fill(i, 255, 255);
-    var x = r * cos(angle);
-    var y = r * sin(angle);
-    stroke(i, 255, 255);
-    line(0, 0, x, y);
-    //vertex(x, y);
-    //var y = map(amp, 0, 256, height, 0);
-    //rect(i * w, y, w - 2, height - y);
+  
+  for(var j = 0; j < 4; j++){
+    for (var i = 0; i < spectrum.length; i++) {
+      var angle = map(i, 0, spectrum.length, 0, 90);
+      var amp = spectrum[i];
+      var r = map(amp, 0, 256, 100, 4*height/9);
+      //fill(i, 255, 255);
+      if (j % 2 == 0) {
+        var x = r * cos(angle+j*90);
+        var y = r * sin(angle+j*90);
+      } else {
+        var x = r * cos(-angle+j*90+90);
+        var y = r * sin(-angle+j*90+90);
+      }
+      var vol = amplitude.getLevel();
+      var ampColor = map(vol, 0, 1, 0, 255)
+      stroke(ampColor+i, 100, 100);
+      line(0, 0, x, y);
+      //vertex(x, y);
+      //var y = map(amp, 0, 256, height, 0);
+      //rect(i * w, y, w - 2, height - y);
+    }
+    //endShape();
   }
-  //endShape();
+  
+  
+  
 
 
 }
